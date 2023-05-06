86fcb7e3e549a3d36132a0900224d4dc44758088
Dylan Buchi <devybuchi@gmail.com>
2023-05-06 15:38:30 -0300

    feat(lib): create TaskList component

---
+++ src/lib/TaskList.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Task } from "../models/task.model";
+  import TaskItem from "./TaskItem.svelte";
+
+  export let tasks: Task[];
+
+  export let onRemoveTask: (event: CustomEvent) => void;
+  export let onToggleComplete: (event: CustomEvent) => void;
+</script>
+
+<ul>
+  {#each tasks as task (task.id)}
+    <li class="mt-5 box {task.completed ? 'has-background-grey-lighter' : ''}">
+      <TaskItem
+        on:toggleComplete={onToggleComplete}
+        on:removeTask={onRemoveTask}
+        {task}
+      />
+    </li>
+  {/each}
+</ul>
