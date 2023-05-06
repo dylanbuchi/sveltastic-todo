eccd9c7114c602649a51bf3cba7fbc29cbfa3402
Dylan Buchi <devybuchi@gmail.com>
2023-05-06 13:03:13 -0300

    feat(lib): create TaskForm component

---
+++ src/lib/TaskForm.svelte
@@ -0,0 +1,48 @@
+<!-- TaskForm.svelte -->
+<script lang="ts">
+  import { nanoid } from "nanoid";
+  import { createEventDispatcher, onMount } from "svelte";
+
+  let taskTitle = "";
+  let inputField: HTMLInputElement = undefined;
+
+  const dispatch = createEventDispatcher();
+
+  function handleSubmit() {
+    if (!taskTitle) return;
+
+    const newTask = {
+      id: nanoid(),
+      title: taskTitle,
+      completed: false,
+    };
+
+    dispatch("addTask", { task: newTask });
+    taskTitle = "";
+    focus();
+  }
+  function focus() {
+    inputField.focus();
+  }
+
+  onMount(focus);
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <div class="field">
+    <div class="control">
+      <input
+        class="input"
+        type="text"
+        placeholder="Enter task"
+        bind:this={inputField}
+        bind:value={taskTitle}
+      />
+    </div>
+  </div>
+  <div class="field">
+    <div class="control">
+      <button class="button is-link" type="submit">Add Task</button>
+    </div>
+  </div>
+</form>
