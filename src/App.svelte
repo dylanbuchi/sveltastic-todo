0c38182fcc0eb07b7fb17d42aac483b0c11f0b5b
Dylan Buchi <devybuchi@gmail.com>
2023-05-06 15:39:50 -0300

    feat(app): add Tasks components

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,35 @@
 <script lang="ts">
+  import TaskForm from "./lib/TaskForm.svelte";
+  import TaskList from "./lib/TaskList.svelte";
+  import type { Task } from "./models/task.model";
+  import { APP_NAME } from "./utils/constants/app.constants";
+
+  let tasks: Task[] = [];
+
+  function handleAddTask(event: CustomEvent) {
+    const { task } = event.detail;
+    tasks = [task, ...tasks];
+  }
+
+  function handleRemoveTask(event: CustomEvent) {
+    const { taskId } = event.detail;
+    tasks = tasks.filter((item) => item.id !== taskId);
+  }
+
+  function handleToggleComplete(event: CustomEvent) {
+    const { taskId } = event.detail;
+    tasks = tasks.map((item) =>
+      item.id === taskId ? { ...item, completed: !item.completed } : item
+    );
+  }
 </script>
+
+<main class="card-content">
+  <h1 class="title">{APP_NAME}</h1>
+  <TaskForm on:addTask={handleAddTask} />
+  <TaskList
+    onRemoveTask={handleRemoveTask}
+    onToggleComplete={handleToggleComplete}
+    {tasks}
+  />
+</main>
