591ae8f2ba0bfbfc201b557c7d263c223521196b
Dylan Buchi <devybuchi@gmail.com>
2023-05-12 17:07:06 -0300

    feat(helpers): add tasks helpers with checkIsAllCompleted and setAllTasks functions

---
+++ src/utils/helpers/tasks.helpers.ts
@@ -0,0 +1,20 @@
+import type { Task } from "../../models/task.model";
+
+export function checkIsAllCompleted(tasks: Task[]) {
+  return tasks?.length > 0 && tasks.every((task) => task.completed);
+}
+
+export function setAllTasks(tasks: Task[], option: "active" | "completed") {
+  const lowerOption = option.toLowerCase();
+  if (lowerOption === "completed") {
+    return tasks.map((task) => ({ ...task, completed: true }));
+  }
+  if (lowerOption === "active") {
+    return tasks.map((task) => ({ ...task, completed: false }));
+  }
+  return tasks;
+}
+
+export function deleteAllCompleted(tasks: Task[]) {
+  return tasks.filter((task) => !task.completed);
+}
