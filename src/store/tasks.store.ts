22de483268a0a8c45727f7201f49f404faf19c59
Dylan Buchi <devybuchi@gmail.com>
2023-05-12 12:25:10 -0300

    feat(store): create derived store to filter tasks and add sorting functions

--- src/store/tasks.store.ts
+++ src/store/tasks.store.ts
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { derived, writable } from "svelte/store";
 import type { Task } from "../models/task.model";
 import {
   loadTasksFromLocalStorage,
@@ -6,6 +6,24 @@ import {
 } from "../utils/helpers/local-storage.helpers";
 
 const initialTasks = loadTasksFromLocalStorage();
+
+function sortByDate(order: string) {
+  const sortOrder = order === "date-desc" ? -1 : 1;
+  return (a: Task, b: Task) => {
+    if (a?.dueDate == null || b?.dueDate == null) return 1;
+
+    return sortOrder * (b.dueDate.getTime() - a.dueDate.getTime());
+  };
+}
+
+function sortByName(order: string) {
+  const sortOrder = order === "name-desc" ? -1 : 1;
+  return (a: Task, b: Task) => {
+    if (a?.title == null || b?.title == null) return 0;
+
+    return sortOrder * a.title.localeCompare(b.title);
+  };
+}
 
 function createTasks() {
   const { subscribe, set, update } = writable<Task[]>(initialTasks);
@@ -42,7 +60,34 @@ function createTasks() {
       set([]);
       saveTasksToLocalStorage([]);
     },
+    sort: (order: string) => {
+      update((tasks) => {
+        const newTasks = [...tasks];
+        if (order === "name-asc" || order === "name-desc") {
+          newTasks.sort(sortByName(order));
+        } else {
+          newTasks.sort(sortByDate(order));
+        }
+
+        return newTasks;
+      });
+    },
   };
 }
 
 export const tasks = createTasks();
+export const taskFilterOption = writable("all");
+
+export const filteredTasks = derived(
+  [tasks, taskFilterOption],
+  ([$tasks, $option]) =>
+    $tasks.filter((item: Task) => {
+      if ($option === "all") {
+        return item;
+      }
+      if ($option === "completed") {
+        return item.completed;
+      }
+      if ($option === "active") return !item.completed;
+    })
+);
