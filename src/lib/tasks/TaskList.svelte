5edffcd22669ebb04a7f8d888406f505840caa43
Dylan Buchi <devybuchi@gmail.com>
2023-05-12 13:14:53 -0300

    refactor(TaskList): use filteredTasks

--- src/lib/tasks/TaskList.svelte
+++ src/lib/tasks/TaskList.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { tasks } from "../../store/tasks.store";
+  import { filteredTasks } from "../../store/tasks.store";
   import TaskItem from "./TaskItem.svelte";
   import autoAnimate from "@formkit/auto-animate";
 </script>
 
 <ul use:autoAnimate>
-  {#each $tasks as task (task.id)}
+  {#each $filteredTasks as task (task.id)}
     <li>
       <TaskItem {task} />
     </li>
