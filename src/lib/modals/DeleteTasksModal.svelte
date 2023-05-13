db9156332054b11ca4cf119d0195c218626a1bf4
Dylan Buchi <devybuchi@gmail.com>
2023-05-13 12:55:01 -0300

    feat(modals): create DeleteTasksModal.svelte

---
+++ src/lib/modals/DeleteTasksModal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { deleteTasksModal } from "../../store/modals.store";
+  import { tasks } from "../../store/tasks.store";
+  import Modal from "./Modal.svelte";
+
+  export let deleteAllCompletedButtonIsClicked = false;
+</script>
+
+<Modal
+  handleCancel={() => {
+    deleteTasksModal.closeModal();
+  }}
+  handleConfirm={() => {
+    if (deleteAllCompletedButtonIsClicked) {
+      tasks.deleteAllCompleted();
+    } else {
+      tasks.clear();
+    }
+
+    deleteTasksModal.closeModal();
+  }}
+  title="Confirmation"
+  message="Are you sure you want to delete all the {deleteAllCompletedButtonIsClicked
+    ? 'completed'
+    : ''} tasks?"
+  confirmButtonText="Delete"
+  cancelButtonText="Cancel"
+  showModal={$deleteTasksModal.isOpen}
+/>
