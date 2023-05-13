509faf0081c514b4a4af9f2461e46a56e709928f
Dylan Buchi <devybuchi@gmail.com>
2023-05-13 12:57:04 -0300

    feat(TaskActions): add DeleteTasksModal component

--- src/lib/tasks/TaskActions.svelte
+++ src/lib/tasks/TaskActions.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+  import { deleteTasksModal } from "../../store/modals.store";
   import { tasks } from "../../store/tasks.store";
-  import { formatTitle } from "../../utils/helpers/string.helpers";
   import {
     checkIsAllCompleted,
     checkSomeAreCompleted,
   } from "../../utils/helpers/tasks.helpers";
+  import DeleteTasksModal from "../modals/DeleteTasksModal.svelte";
 
   let option: "active" | "completed";
+
+  $: deleteAllCompletedButtonIsClicked = false;
 
   $: {
     option = checkIsAllCompleted($tasks) ? "active" : "completed";
@@ -16,18 +19,19 @@
 <section class="box p-5 is-flex is-flex-direction-column">
   <h2 class="title is-5 has-text-centered">Manage Tasks</h2>
   <button
-    on:click={() => tasks.transform(option)}
-    class="button is-fullwidth is-link"
+    on:click={deleteTasksModal.openModal}
+    class="mt-2 button is-fullwidth is-danger"
   >
-    Set All {formatTitle(option)}</button
-  >
-  <button on:click={tasks.clear} class="mt-2 button is-fullwidth is-danger">
     Delete All</button
   >
   {#if checkSomeAreCompleted($tasks)}
     <button
-      on:click={tasks.deleteAllCompleted}
+      on:click={() => {
+        deleteAllCompletedButtonIsClicked = true;
+        deleteTasksModal.openModal();
+      }}
       class="mt-2 button is-fullwidth is-danger">Delete All Completed</button
     >
   {/if}
+  <DeleteTasksModal {deleteAllCompletedButtonIsClicked} />
 </section>
