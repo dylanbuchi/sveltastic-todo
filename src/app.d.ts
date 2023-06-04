866436de3398fb56c8fa8866f4963d2c8c932c80
Dylan Buchi <devybuchi@gmail.com>
2023-06-04 17:43:15 -0300

    feat(app): set up Prisma and Lucia auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,19 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+		interface Locals {
+			auth: import('lucia-auth').AuthRequest;
+		}
+	}
+}
+
+/// <reference types="lucia-auth" />
+declare global {
+	namespace Lucia {
+		type Auth = import('@/server/lucia').Auth;
+		type UserAttributes = {
+			name?: string;
+			email?: string;
+		};
 	}
 }
 
---
+++ src/server/lucia.ts
@@ -0,0 +1,15 @@
+import lucia from 'lucia-auth';
+import { sveltekit } from 'lucia-auth/middleware';
+import prisma from '@lucia-auth/adapter-prisma';
+import { dev } from '$app/environment';
+import { prismaClient } from '@/services/prisma';
+
+export const auth = lucia({
+	adapter: prisma(prismaClient),
+	env: dev ? 'DEV' : 'PROD',
+	middleware: sveltekit(),
+	transformDatabaseUser: (userData) => {
+		return { userId: userData.id, email: userData.email, name: userData.name };
+	}
+});
+export type Auth = typeof auth;
