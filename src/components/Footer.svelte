cf261c62580c9e827fbc249bfc5590c90e422111
Dylan Buchi <devybuchi@gmail.com>
2023-06-03 21:15:21 -0300

    fix(Footer): remove onMount

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { APP_NAME } from '../utils/constants/app.constants';
 
-	let currentYear: number;
-
-	onMount(() => (currentYear = new Date().getFullYear()));
+	let currentYear = new Date().getFullYear();
 </script>
 
 <footer class="footer p-6">
