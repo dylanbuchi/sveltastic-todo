1038a68849737056df8e7c0f482ac13867f7294a
Dylan Buchi <devybuchi@gmail.com>
2023-06-06 13:02:35 -0300

    feat(login): add error messages and form enhancement to handle login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+
+	$: errorMessage = '';
 </script>
 
 <div class="container py-4">
 	<h1 class="title is-4">Log in to your account</h1>
-	<form method="POST" use:enhance>
+	<form
+		method="POST"
+		use:enhance={() => {
+			return async ({ result, update }) => {
+				if (result.type === 'success') {
+				} else if (result.type === 'failure') {
+					const error = result?.data?.error ?? 'Something went wrong';
+
+					if (error === 'AUTH_INVALID_KEY_ID') {
+						errorMessage = `Sorry, we couldn't find an account associated with this email.`;
+					} else if (error === 'AUTH_INVALID_PASSWORD') {
+						errorMessage = 'Invalid password';
+					} else {
+						errorMessage = error;
+					}
+				}
+				update();
+			};
+		}}
+	>
+		{#if errorMessage}
+			<div class="notification is-danger has-text-centered p-4">{errorMessage}</div>
+		{/if}
 		<div class="field">
 			<label class="label" for="email">Email</label>
 			<div class="control">
