9f752f9ac9d46ec9b00918e91e571e946aa69b6c
Dylan Buchi <devybuchi@gmail.com>
2023-06-10 23:34:56 -0300

    feat(app): add loading spinner

---
+++ src/components/Spinner.svelte
@@ -0,0 +1,36 @@
+<div class="loader-overlay">
+	<div class="loader" />
+</div>
+
+<style>
+	.loader-overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(255, 255, 255, 0);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: 9999;
+	}
+
+	.loader {
+		border: 20px solid #eaf0f6;
+		border-radius: 50%;
+		border-top: 20px solid #66d7b3;
+		width: 120px;
+		height: 120px;
+		animation: spinner 1.5s linear infinite;
+	}
+
+	@keyframes spinner {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import Spinner from '@/components/Spinner.svelte';
 
 	$: errorMessage = '';
 	$: isGuest = false;
@@ -8,59 +9,64 @@
 
 <div class="container py-4">
 	<h1 class="title is-4">Log in to your account</h1>
-	<form
-		method="POST"
-		use:enhance={({ action }) => {
-			if (isLoading) return;
-			isLoading = true;
-			isGuest && (action.href = action.href + '?guest');
+	{#if errorMessage}
+		<div class="notification is-danger has-text-centered p-4">{errorMessage}</div>
+	{/if}
 
-			return async ({ result, update }) => {
-				if (result.type === 'failure') {
-					isLoading = false;
-					const error = result?.data?.error ?? 'Something went wrong';
+	{#if isLoading}
+		<Spinner />
+	{:else}
+		<form
+			method="POST"
+			use:enhance={({ action }) => {
+				if (isLoading) return;
+				isLoading = true;
+				isGuest && (action.href = action.href + '?guest');
 
-					if (error === 'AUTH_INVALID_KEY_ID') {
-						errorMessage = `Sorry, we couldn't find an account associated with this email.`;
-					} else if (error === 'AUTH_INVALID_PASSWORD') {
-						errorMessage = 'Invalid password';
-					} else {
-						errorMessage = error;
+				return async ({ result, update }) => {
+					if (result.type === 'failure') {
+						isLoading = false;
+						const error = result?.data?.error ?? 'Something went wrong';
+
+						if (error === 'AUTH_INVALID_KEY_ID') {
+							errorMessage = `Sorry, we couldn't find an account associated with this email.`;
+						} else if (error === 'AUTH_INVALID_PASSWORD') {
+							errorMessage = 'Invalid password';
+						} else {
+							errorMessage = error;
+						}
 					}
-				}
-				update();
-			};
-		}}
-	>
-		{#if errorMessage}
-			<div class="notification is-danger has-text-centered p-4">{errorMessage}</div>
-		{/if}
-		<div class="field">
-			<label class="label" for="email">Email</label>
-			<div class="control">
-				<input class="input" type="email" id="email" name="email" />
+					update();
+				};
+			}}
+		>
+			<div class="field">
+				<label class="label" for="email">Email</label>
+				<div class="control">
+					<input class="input" type="email" id="email" name="email" />
+				</div>
 			</div>
-		</div>
-		<div class="field">
-			<label class="label" for="password">Password</label>
-			<div class="control">
-				<input class="input" type="password" id="password" name="password" />
+			<div class="field">
+				<label class="label" for="password">Password</label>
+				<div class="control">
+					<input class="input" type="password" id="password" name="password" />
+				</div>
 			</div>
-		</div>
-		<div class="field is-flex is-align-items-center is-justify-content-space-between">
-			<div class="control">
-				<input disabled={isLoading} class="button is-primary" type="submit" value="Log in" />
+			<div class="field is-flex is-align-items-center is-justify-content-space-between">
+				<div class="control">
+					<input disabled={isLoading} class="button is-primary" type="submit" value="Log in" />
+				</div>
+				<div>or</div>
+				<div class="control">
+					<input
+						disabled={isLoading}
+						on:click={() => (isGuest = true)}
+						class="button is-link"
+						type="submit"
+						value="Log in as Guest"
+					/>
+				</div>
 			</div>
-			<div>or</div>
-			<div class="control">
-				<input
-					disabled={isLoading}
-					on:click={() => (isGuest = true)}
-					class="button is-link"
-					type="submit"
-					value="Log in as Guest"
-				/>
-			</div>
-		</div>
-	</form>
+		</form>
+	{/if}
 </div>
