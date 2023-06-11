d194d879d21c508e966e1a68549571022b491a90
Dylan Buchi <devybuchi@gmail.com>
2023-06-10 21:35:44 -0300

    feat(constants): add guest task titles

--- src/utils/constants/tasks.constants.ts
+++ src/utils/constants/tasks.constants.ts
@@ -20,3 +20,16 @@ export const taskFilterOptionToName: Record<TaskFilterOption, string> = {
 	completed: 'Completed tasks',
 	expired: 'Expired tasks'
 };
+
+export const guestTaskTitles: string[] = [
+	'Conquer the World',
+	'Unlock Hidden Powers',
+	'Master the Art of Coding',
+	'Explore the Unknown',
+	'Build a Time Machine',
+	'Create a Masterpiece',
+	'Become a Superhero',
+	'Discover the Meaning of Life',
+	'Conquer Fear and Doubt',
+	'Achieve Ultimate Enlightenment'
+];
