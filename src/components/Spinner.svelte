4ed37c595b6b5371346879d75bb75729ab7f9b40
Dylan Buchi <devybuchi@gmail.com>
2023-06-18 11:33:52 -0300

    feat(Spinner): add loading status texts

--- src/components/Spinner.svelte
+++ src/components/Spinner.svelte
@@ -1,9 +1,70 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
 	import { isDarkMode } from '@/store/theme.store';
+	import { loginAsGuestClicked } from '@/store/app.store';
+
+	let secondsElapsed = 0;
+	let loadingStatus = 'Setting up your account...';
+
+	let timer: NodeJS.Timer;
+
+	function updateLoadingScreen() {
+		switch (secondsElapsed) {
+			case 2:
+				loadingStatus = 'Loading the guest features...';
+				break;
+			case 4:
+				loadingStatus = 'Building a personalized environment for you...';
+				break;
+			case 6:
+				loadingStatus = 'Getting things ready just for you...';
+				break;
+			case 8:
+				loadingStatus = 'Creating a seamless guest experience...';
+				break;
+			case 10:
+				loadingStatus = 'Setting the stage for your journey...';
+				break;
+			case 12:
+				loadingStatus = 'Loading the necessary resources...';
+				break;
+			case 14:
+				loadingStatus = 'Crafting a unique guest session...';
+				break;
+			case 16:
+				loadingStatus = 'Building the foundation of your visit...';
+				break;
+		}
+	}
+
+	function startGuestLoginLoadingProcess() {
+		timer = setInterval(() => {
+			secondsElapsed++;
+			updateLoadingScreen();
+		}, 1000);
+	}
+
+	onMount(() => {
+		if ($loginAsGuestClicked) startGuestLoginLoadingProcess();
+	});
+
+	onDestroy(() => {
+		clearInterval(timer);
+		$loginAsGuestClicked = false;
+	});
 </script>
 
-<div class:card-dark={$isDarkMode} class:bg={!$isDarkMode} class="loader-overlay">
+<div
+	class:card-dark={$isDarkMode}
+	class:bg={!$isDarkMode}
+	class="is-flex is-flex-direction-column loader-overlay is-align-content-center is-justify-content-center"
+>
 	<div class="loader" />
+	{#if $loginAsGuestClicked}
+		<h1 class:text-dark={$isDarkMode} class="px-4 mt-6 title is-size-5-mobile">
+			{loadingStatus}
+		</h1>
+	{/if}
 </div>
 
 <style>
